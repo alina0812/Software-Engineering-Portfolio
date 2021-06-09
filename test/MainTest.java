a518e9cc7fed6969563579e5bb4fcedb8decbe1b
Alina Brockmann <alina.brockmann@sap.com>
2021-06-09 15:40:32 +0200

    test package

---
+++ test/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void main() {
+    assertEquals(10, Main.add(4,6));
+  }
+}
