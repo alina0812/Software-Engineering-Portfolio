d5706f05dd7ccd63d8cf9e03cd4202116748d320
crisptells <cochamer@gmx.de>
2021-06-11 17:57:01 +0200

    - Reworked WriteJson Class, so that JSON file is structured and in english - Added Classes for Model, Engine, Transmission and Seat. - Removed old ReadJson from applicationLayer package - Added New ReadJson with methods: load_data, getModels, getEngines, getTransmissions and getSeats in package dataLayer

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import datalayer_test.ReadJson;
 import presentationLayer.Controller;
 import presentationLayer.Model;
 import presentationLayer.View;
@@ -5,14 +6,18 @@ import presentationLayer.View;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
-    System.out.println("Test");
-    System.out.println("Hallo");
     System.out.println(add(2, 5));
 
     View view = new View();
     Model model = new Model();
     Controller controller = new Controller(view, model);
+
+    ReadJson.load_data(); // loads data from JSON File
+    System.out.println(ReadJson.getModels());
+    System.out.println(ReadJson.getEngines());
+    System.out.println(ReadJson.getTransmissions());
+    System.out.println(ReadJson.getSeats());
+
   }
 
   public static int add(int a, int b) {
